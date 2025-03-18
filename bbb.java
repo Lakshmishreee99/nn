db71cdff1c998f01cab52c3e9304bcc43a2bb7f6
Lakshmishreee99 <vvce22cseaiml0138@vvce.ac.in>
2025-03-18 12:15:34 +0530

    Update and rename bbb.java to bbb.py

--- bbb.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-  pubic static void main(String[] args){
-   int length=5;
-   int width=5;
-   int half=5;
-  int area=length*width*half;
-System.out.println("area of tringle"+area);
-}
-}
