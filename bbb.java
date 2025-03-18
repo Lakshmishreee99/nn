29f4546b0eac08a0be6e6f2e1d57f7369cb4afb6
Lakshmishreee99 <vvce22cseaiml0138@vvce.ac.in>
2025-03-18 12:11:12 +0530

    Create bbb.java

---
+++ bbb.java
@@ -0,0 +1,9 @@
+public class Main{
+  pubic static void main(String[] args){
+   int length=5;
+   int width=5;
+   int half=5;
+  int area=length*width*half;
+System.out.println("area of tringle"+area);
+}
+}
